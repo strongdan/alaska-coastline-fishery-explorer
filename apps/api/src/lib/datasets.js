e7b87816aaefd034393be724e10a4b5415dac1cb
Daniel Strong <daniel.strong@alaska.gov>
2026-04-24 18:14:40 -0800

    feat: expand regional coverage with Yakutat and granular management layers for PWS, BB, and UCI

--- apps/api/src/lib/datasets.js
+++ apps/api/src/lib/datasets.js
@@ -1,18 +1,68 @@
 module.exports = {
   pws: {
     id: "pws",
-    name: "Prince William Sound & Copper River",
+    name: "Prince William Sound Districts",
     url: "https://services.arcgis.com/VdkVOAHovLuozJG4/arcgis/rest/services/Commercial_Salmon_PrinceWilliamSound_Districts/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson"
   },
-  "bristol-bay": {
-    id: "bristol-bay",
-    name: "Bristol Bay",
-    url: "https://gis.adfg.alaska.gov/arcgis/rest/services/CF_public/BristolBaySalmon5AAC_06_200/MapServer/8/query?outFields=*&where=1%3D1&f=geojson"
+  "pws-stat-areas": {
+    id: "pws-stat-areas",
+    name: "Prince William Sound Statistical Areas",
+    url: "https://services.arcgis.com/VdkVOAHovLuozJG4/arcgis/rest/services/Commercial_Salmon_PrinceWilliamSound_StatisticalAreas/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson"
   },
-  "upper-cook-inlet": {
-    id: "upper-cook-inlet",
-    name: "Upper Cook Inlet",
-    url: "https://gis.adfg.alaska.gov/arcgis/rest/services/CF_public/CookInlet_5AAC_21_200/MapServer/3/query?outFields=*&where=1%3D1&f=geojson"
+  "pws-subdistricts": {
+    id: "pws-subdistricts",
+    name: "Prince William Sound Subdistricts",
+    url: "https://services.arcgis.com/VdkVOAHovLuozJG4/arcgis/rest/services/Commercial_Salmon_PrinceWilliamSound_Subdistricts/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson"
+  },
+  "pws-registration-areas": {
+    id: "pws-registration-areas",
+    name: "Prince William Sound Registration Areas",
+    url: "https://services.arcgis.com/VdkVOAHovLuozJG4/arcgis/rest/services/PrinceWilliamSound_Salmon_RegistrationAreas/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson"
+  },
+  "bristol-bay-districts": {
+    id: "bristol-bay-districts",
+    name: "Bristol Bay Districts",
+    url: "https://gis.adfg.alaska.gov/ags/rest/services/CF_public/BristolBaySalmon5AAC_06_200/MapServer/8/query?where=1%3D1&outFields=*&f=geojson"
+  },
+  "bristol-bay-sections": {
+    id: "bristol-bay-sections",
+    name: "Bristol Bay Sections",
+    url: "https://gis.adfg.alaska.gov/ags/rest/services/CF_public/BristolBaySalmon5AAC_06_200/MapServer/7/query?where=1%3D1&outFields=*&f=geojson"
+  },
+  "bristol-bay-stat-areas": {
+    id: "bristol-bay-stat-areas",
+    name: "Bristol Bay Statistical Areas",
+    url: "https://gis.adfg.alaska.gov/ags/rest/services/CF_public/BristolBaySalmon5AAC_06_200/MapServer/6/query?where=1%3D1&outFields=*&f=geojson"
+  },
+  "bristol-bay-registration-areas": {
+    id: "bristol-bay-registration-areas",
+    name: "Bristol Bay Registration Areas",
+    url: "https://gis.adfg.alaska.gov/ags/rest/services/CF_public/BristolBaySalmon5AAC_06_200/MapServer/9/query?where=1%3D1&outFields=*&f=geojson"
+  },
+  "upper-cook-inlet-districts": {
+    id: "upper-cook-inlet-districts",
+    name: "Upper Cook Inlet Districts",
+    url: "https://gis.adfg.alaska.gov/ags/rest/services/CF_public/CookInlet_5AAC_21_200/MapServer/3/query?where=1%3D1&outFields=*&f=geojson"
+  },
+  "upper-cook-inlet-subdistricts": {
+    id: "upper-cook-inlet-subdistricts",
+    name: "Upper Cook Inlet Subdistricts",
+    url: "https://gis.adfg.alaska.gov/ags/rest/services/CF_public/CookInlet_5AAC_21_200/MapServer/2/query?where=1%3D1&outFields=*&f=geojson"
+  },
+  "upper-cook-inlet-sections": {
+    id: "upper-cook-inlet-sections",
+    name: "Upper Cook Inlet Sections",
+    url: "https://gis.adfg.alaska.gov/ags/rest/services/CF_public/CookInlet_5AAC_21_200/MapServer/1/query?where=1%3D1&outFields=*&f=geojson"
+  },
+  "upper-cook-inlet-stat-areas": {
+    id: "upper-cook-inlet-stat-areas",
+    name: "Upper Cook Inlet Statistical Areas",
+    url: "https://gis.adfg.alaska.gov/ags/rest/services/CF_public/CookInlet_5AAC_21_200/MapServer/0/query?where=1%3D1&outFields=*&f=geojson"
+  },
+  "upper-cook-inlet-drift-gillnet": {
+    id: "upper-cook-inlet-drift-gillnet",
+    name: "Upper Cook Inlet Drift Gillnet Areas 1-4",
+    url: "https://gis.adfg.alaska.gov/ags/rest/services/CF_public/CookInlet_5AAC_21_200/MapServer/4/query?where=1%3D1&outFields=*&f=geojson"
   },
   "southeast-districts": {
     id: "southeast-districts",
@@ -28,6 +78,16 @@ module.exports = {
     id: "southeast-stat-areas",
     name: "Southeast Alaska Statistical Areas",
     url: "https://gis.adfg.alaska.gov/ags/rest/services/CF_public/SoutheastSalmon5AAC33_200/MapServer/9/query?where=1%3D1&outFields=*&f=geojson"
+  },
+  "yakutat-districts": {
+    id: "yakutat-districts",
+    name: "Yakutat Districts",
+    url: "https://gis.adfg.alaska.gov/ags/rest/services/CF_public/Yakutat_Commercial_Salmon_Fishing_Districts_and_Stat_Areas_5_AAC_30_200/MapServer/5/query?where=1%3D1&outFields=*&f=geojson"
+  },
+  "yakutat-stat-areas": {
+    id: "yakutat-stat-areas",
+    name: "Yakutat Statistical Areas",
+    url: "https://gis.adfg.alaska.gov/ags/rest/services/CF_public/Yakutat_Commercial_Salmon_Fishing_Districts_and_Stat_Areas_5_AAC_30_200/MapServer/4/query?where=1%3D1&outFields=*&f=geojson"
   },
   "alaska-communities": {
     id: "alaska-communities",
